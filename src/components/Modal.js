7b0e38a4457479ca18570d13b102c81a7d80220f
jyansol <hahn0406@gmail.com>
2019-01-22 23:29:43 +0900

    [FEAT] Modal component 띄우기

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 export default class Modal extends Component {
   render() {
+    const { isOpen, onClose } = this.props;
     return (
       <>
-        <div className="modal" />
+        <div className={isOpen ? 'modal modal--is-open' : 'modal'}>
+          <button onClick={onClose}>close</button>
+          <p>Hello! I am a modal.</p>
+        </div>
       </>
     );
   }
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,15 +3,24 @@ import grim from '../images/grim.png';
 import baseball from '../images/baseball.png';
 import baeminchan from '../images/baeminchan.png';
 import project from './_project.scss';
-import { Link } from 'react-router-dom';
+import Modal from './Modal.js';
 
 export default class Project extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      display: false,
+      isModalOpen: false,
     };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ isModalOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ isModalOpen: false });
   }
 
   render() {
@@ -20,9 +29,11 @@ export default class Project extends Component {
         <div class="Project">
           <h1 className="title">Project</h1>
           <div className="Project__img">
-            <Link src={baeminchan} alt="" />
+            {/* <Modal onClick={this.openModal} /> */}
+            <Modal onClick={this.openModal} isOpen={this.state.isModalOpen} onClose={this.closeModal} />
+            {/* <a onClick={this.openModal}>Open the modal</a>
             <img src={grim} alt="" />
-            <img src={baseball} alt="" />
+            <img src={baseball} alt="" /> */}
           </div>
         </div>
       </>
