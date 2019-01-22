4727549a37a245ca3852f5d17080ca5c797ea05c
jyansol <hahn0406@gmail.com>
2019-01-22 20:03:27 +0900

    [FEAT] Modal component 생성

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,15 +3,24 @@ import grim from '../images/grim.png';
 import baseball from '../images/baseball.png';
 import baeminchan from '../images/baeminchan.png';
 import project from './_project.scss';
+import { Link } from 'react-router-dom';
 
 export default class Project extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      display: false,
+    };
+  }
+
   render() {
     return (
       <>
         <div class="Project">
           <h1 className="title">Project</h1>
           <div className="Project__img">
-            <img src={baeminchan} alt="" />
+            <Link src={baeminchan} alt="" />
             <img src={grim} alt="" />
             <img src={baseball} alt="" />
           </div>
